76e4d537cefb65fe7742fd25a21c5b53032b1d57
beafig <beafigueroa89@gmail.com>
2023-09-26 12:12:58 +0200

    Add ProductDao and ContactDao

---
+++ src/main/java/com/campusdual/appmazing/model/dao/ProductDao.java
@@ -0,0 +1,11 @@
+package com.campusdual.appmazing.model.dao;
+
+import com.campusdual.appmazing.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// el mismo nombre del modelo añadiendo Dao (Data Access Object)
+// lleva un mapa interno en el que se indica el tipo de dato de la clase Product, y el tipo de
+// dato de la clave primaria, que sería int, pero no admite datos primitivos por eso usamos Integer
+public interface ProductDao extends JpaRepository<Product, Integer> {
+
+}
