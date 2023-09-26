b9cdc93c5503111d90b04ef0711e5d61fa621328
beafig <beafigueroa89@gmail.com>
2023-09-26 11:33:57 +0200

    Create class Contact

---
+++ src/main/java/com/campusdual/appmazing/model/Contact.java
@@ -0,0 +1,69 @@
+package com.campusdual.appmazing.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "CONTACTS")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String first_surname;
+    @Column
+    private String second_surname;
+    @Column
+    private int phone_number;
+    @Column
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirst_surname() {
+        return first_surname;
+    }
+
+    public void setFirst_surname(String first_surname) {
+        this.first_surname = first_surname;
+    }
+
+    public String getSecond_surname() {
+        return second_surname;
+    }
+
+    public void setSecond_surname(String second_surname) {
+        this.second_surname = second_surname;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
