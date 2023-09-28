db09a7de7c486947c33425fa557474715ae96b43
beafig <beafigueroa89@gmail.com>
2023-09-28 14:19:29 +0200

    Create contact controller to make request

---
+++ src/main/java/com/campusdual/appmazing/controller/ContactController.java
@@ -0,0 +1,63 @@
+package com.campusdual.appmazing.controller;
+
+import com.campusdual.appmazing.api.IContactService;
+import com.campusdual.appmazing.model.Contact;
+import com.campusdual.appmazing.model.dto.ContactDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@RestController: indico que esto es un controlador REST (para hacer peticiones GET, PUT, POST,
+// DELETE)
+//RequestMapping: indica la url donde se hace la petición, pero no completa, sino el endpoint
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+    @Autowired
+    IContactService contactService;
+
+    //@GetMapping: indico que es una petición de tipo GET
+    @GetMapping
+    public String testController() {
+        return "Contact controller works";
+    }
+
+    @PostMapping
+    public String testController(@RequestBody String name) {
+        return "Contact controller works, " + name + "!";
+    }
+
+    @GetMapping(value = "/testMethod")
+    public String testControllerMethod() {
+        return "Contact controller method works";
+    }
+
+    // método que devuelve un DTO pasándole el id
+    @PostMapping(value = "/get")
+    public ContactDTO queryContact (@RequestBody  ContactDTO contactDTO){
+        return this.contactService.queryContact(contactDTO);
+    }
+
+    @GetMapping(value ="/getAll")
+    public List<ContactDTO> queryAllContacts(){
+        return this.contactService.queryAllContacts();
+    }
+
+    @PostMapping(value = "/add")
+    public int insertContact(@RequestBody ContactDTO contactDTO){
+        return this.contactService.insertContact(contactDTO);
+    }
+
+    // hay que indicar el id para que no cree un elemento nuevo, si no que modifique el
+    // seleccionado
+    @PutMapping(value = "/update")
+    public int updateContact(@RequestBody ContactDTO contactDTO){
+        return this.contactService.updateContact(contactDTO);
+    }
+
+    @DeleteMapping (value = "/remove")
+    public int deleteContact(@RequestBody ContactDTO contactDTO){
+        return this.contactService.deleteContact(contactDTO);
+    }
+}
