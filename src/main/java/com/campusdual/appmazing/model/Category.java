732262bfd126114fecfcac0e653ee2239902cb73
beafig <beafigueroa89@gmail.com>
2023-09-29 09:04:51 +0200

    Add new table Category and create model

---
+++ src/main/java/com/campusdual/appmazing/model/Category.java
@@ -0,0 +1,29 @@
+package com.campusdual.appmazing.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CATEGORY")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
